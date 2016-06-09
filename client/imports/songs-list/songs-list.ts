8acd6ec5558c5cf8dba00270678a379c64e3ce0f
bhanu <bhanu@successivesoftwares.com>
2016-06-09 17:04:30 +0530

    Pagination

--- client/imports/songs-list/songs-list.ts
+++ client/imports/songs-list/songs-list.ts
@@ -7,10 +7,14 @@ import 'zone.js/dist/zone';
 import { Component }   from '@angular/core';
 import { Songs }     from '../../../collections/songs';
 import { Mongo }       from 'meteor/mongo';
+import {PaginatePipe, PaginationControlsCmp, PaginationService} from 'ng2-pagination';
 
 @Component({
     selector: 'parties-list',
-    templateUrl: '/client/imports/songs-list/songs-list.html'
+    templateUrl: '/client/imports/songs-list/songs-list.html',
+    directives: [PaginationControlsCmp],
+    pipes: [PaginatePipe],
+    providers: [PaginationService]
 })
 export class SongsList {
     songs: Mongo.Cursor<Object>;
